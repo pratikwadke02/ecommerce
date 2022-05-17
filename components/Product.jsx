a7ba2e96682073f83c6bbbba91a6be09edc07937
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-05-17 19:59:50 +0530

    Ui done

--- components/Product.jsx
+++ components/Product.jsx
@@ -6,12 +6,13 @@ const Product = ({product : {image, name, slug, price}}) => {
   return (
     <>
     <div>
-      <Link href = {`/product/${slug.current}`}><a></a></Link>
+      <Link href = {`/product/${slug.current}`}>
       <div className="product-card">
         <img src={urlFor(image && image[0])} width={250} height={250} alt="product_image" className='product-image'/>
         <p className="product-name">{name}</p>
         <p className="product-price">${price}</p>
       </div>
+      </Link>
     </div>
     </>
   )
